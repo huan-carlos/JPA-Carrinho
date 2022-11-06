9243c9c93d0162d9d297abac442a73691b5893d2
Huan Carlos Cantuaria Vilarins <63238005+huan-carlos@users.noreply.github.com>
2022-11-06 19:39:30 -0300

    Passando atribuições pasa subclasse

--- src/main/java/com/example/CRUDProdutoJPA/model/entity/Pessoa.java
+++ src/main/java/com/example/CRUDProdutoJPA/model/entity/Pessoa.java
@@ -1,37 +1,22 @@
 package com.example.CRUDProdutoJPA.model.entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Scope(value = "session")
 @Component
 @Entity
-@Table(name = "tb_cliente")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Pessoa implements Serializable {
 
     @GeneratedValue(strategy = GenerationType.TABLE)
     @Id
     private long id;
-
-    @OneToMany(mappedBy = "pessoa")
-    private List<Venda> compras;
-
-    public Pessoa() {
-        this.compras = new ArrayList<>();
-    }
 
     public long getId() {
         return id;
--- src/main/java/com/example/CRUDProdutoJPA/model/entity/PessoaFisica.java
+++ src/main/java/com/example/CRUDProdutoJPA/model/entity/PessoaFisica.java
@@ -1,16 +1,31 @@
 package com.example.CRUDProdutoJPA.model.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope(value = "session")
+@Component
 @Entity
 @Table(name = "tb_pessoafisica")
 public class PessoaFisica extends Pessoa implements Serializable {
 
     private String nome;
     private String cpf;
+
+    @OneToMany(mappedBy = "pessoa")
+    private List<Venda> compras;
+
+    public PessoaFisica() {
+        this.compras = new ArrayList<>();
+    }
 
     public String getNome() {
         return nome;
@@ -26,5 +41,13 @@ public class PessoaFisica extends Pessoa implements Serializable {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public List<Venda> getCompras() {
+        return compras;
+    }
+
+    public void addCompra(Venda venda) {
+        this.compras.add(venda);
     }
 }
